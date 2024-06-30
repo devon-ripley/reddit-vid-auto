8f67660f53aef08b719b93bae7e425eb8800c835
Devon Ripley <derippet@gmail.com>
2024-06-30 05:18:41 -0600

    config.json auto creation if not found

--- base_config.py
+++ base_config.py
@@ -1,10 +1,13 @@
+import json
+
+
 class Config:
     def __init__(self):
         self.client_id = 'ggffsssjjj'
         self.client_secret = 'jjjjhhhhhggg555'
         self.user_agent = 'reader'
 
-        #story vars auto load
+        # story vars auto load
         self.number_of_posts = 1
         self.sub_reddits = ['relationship_advice']
 
@@ -14,8 +17,7 @@ class Config:
         self.story_delim = '#'
         self.target_story = ['1dnt7ar', '1dnu3y3', '1dnkv2i']
 
-
-        #vid vars
+        # vid vars
         self.start_delay = 10
         self.fps = 30
         self.resolution = (1920, 1080)
@@ -36,36 +38,73 @@ class Config:
 
 
 def setup():
-    with open('config.json') as json_file:
-        data = json.load(json_file)
-    config = Config()
-    config.client_id = data["client_id"]
-    config.client_secret = data["client_secret"]
-    config.user_agent = data["user_agent"]
+    try:
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+        config = Config()
+        config.client_id = data["client_id"]
+        config.client_secret = data["client_secret"]
+        config.user_agent = data["user_agent"]
 
-    # story vars autoload
-    config.number_of_posts = data["number_of_posts"]
-    config.sub_reddits = data["sub_reddits"]
+        # story vars autoload
+        config.number_of_posts = data["number_of_posts"]
+        config.sub_reddits = data["sub_reddits"]
 
-    # comment vars
-    config.number_of_comments = data["number_of_comments"]
-    config.story_delim_audio = data["story_delim_audio"]
-    config.story_delim = data["story_delim"]
-    config.target_story = data["target_story"]
+        # comment vars
+        config.number_of_comments = data["number_of_comments"]
+        config.story_delim_audio = data["story_delim_audio"]
+        config.story_delim = data["story_delim"]
+        config.target_story = data["target_story"]
 
-    # vid vars
-    config.start_delay = data["start_delay"]
-    config.fps = data["fps"]
-    config.resolution = data["resolution"]
-    config.resolution_back = data["resolution_back"]
-    config.vertical_resolution = data["vertical_resolution"]
-    config.vertical_resolution_back = data["vertical_resolution_back"]
-    config.font = data["font"]
-    # Trebuchet-MS-Bold, Segoe-UI-Semibold 'to wide' , Arial-black is great, a little sharp
-    config.text_size = data["text_size"]
-    config.text_color = data["text_color"]
-    config.stroke_color = data["stroke_color"]
-    config.stroke_size = data["stroke_size"]
-    config.align = data["align"]
-    config.tts_to_use = data["tts_to_use"]
+        # vid vars
+        config.start_delay = data["start_delay"]
+        config.fps = data["fps"]
+        config.resolution = data["resolution"]
+        config.resolution_back = data["resolution_back"]
+        config.vertical_resolution = data["vertical_resolution"]
+        config.vertical_resolution_back = data["vertical_resolution_back"]
+        config.font = data["font"]
+        # Trebuchet-MS-Bold, Segoe-UI-Semibold 'to wide' , Arial-black is great, a little sharp
+        config.text_size = data["text_size"]
+        config.text_color = data["text_color"]
+        config.stroke_color = data["stroke_color"]
+        config.stroke_size = data["stroke_size"]
+        config.align = data["align"]
+        config.tts_to_use = data["tts_to_use"]
+
+    except:
+        print("No config.json file found, creating new one")
+        config = Config()
+        data = {}
+        data["client_id"] = config.client_id
+        data["client_secret"] = config.client_secret
+        data["user_agent"] = config.user_agent
+
+        # story vars auto load
+        data["number_of_posts"] = config.number_of_posts
+        data["sub_reddits"] = config.sub_reddits
+
+        # comment vars
+        data["number_of_comments"] = config.number_of_comments
+        data["story_delim_audio"] = config.story_delim_audio
+        data["story_delim"] = config.story_delim
+        data["target_story"] = config.target_story
+
+        # vid vars
+        data["start_delay"] = config.start_delay
+        data["fps"] = config.fps
+        data["resolution"] = config.resolution
+        data["resolution_back"] = config.resolution_back
+        data["vertical_resolution"] = config.vertical_resolution
+        data["vertical_resolution_back"] = config.vertical_resolution_back
+        data["font"] = config.font
+        # Trebuchet-MS-Bold, Segoe-UI-Semibold 'to wide' , Arial-black is great, a little sharp
+        data["text_size"] = config.text_size
+        data["text_color"] = config.text_color
+        data["stroke_color"] = config.stroke_color
+        data["stroke_size"] = config.stroke_size
+        data["align"] = config.align
+        data["tts_to_use"] = config.tts_to_use
+        with open('config.json', "w") as outfile:
+            json.dump(data, outfile, indent=3)
     return config
